87a60f61f19dc68fa1857064564b2b06e217497a
Hariom Kabra <143120733+Hariomkabra@users.noreply.github.com>
2024-09-09 10:46:22 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+//alert("Hello") // we are using node.js ,not browser
+
+// code readability should be high 
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn=false
+let state;
+
+//premative datatype's
+//number=> t to power 53
+// bigint 
+// string => ""
+// boolean=> true/false
+//null=> standalone value/ empty
+//undefind=> value is assign still
+//symbol => unique
+
+//object
+
+console.log(typeof "hariom"); //string
+
+console.log(typeof age); //number
+
+console.log(typeof null); //object
+ 
+console.log(typeof undefined); //undefind
+
