1b1c7ad505e0e0e9989859d4edc5274a4c9dbfdf
Hariom Kabra <143120733+Hariomkabra@users.noreply.github.com>
2024-09-09 10:17:16 +0000

    basics sentax

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,20 @@
+const accountId = 786786
+let accountEmail ="hariom07@gmail.com"
+var accountPassword ='12345'
+accountCity="chh.Sam.Nagar"
+let accountState;
+
+//accountID = 8989 // not allowed
+
+accountEmail ="sarang@gmail.com"
+accountPassword="121212"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+prefer not to use the var
+because issue in block scope and fuctional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
