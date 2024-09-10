1f3bc08649f769281617778a6fa808f2e29a1033
Hariom Kabra <143120733+Hariomkabra@users.noreply.github.com>
2024-09-10 11:00:30 +0000

    Conversion of Datatype

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,95 @@
+/*let score ='33'
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number (score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+o/p
+string
+string
+number
+33 */
+
+/*let age =null
+console.log(typeof age);
+console.log(typeof (age));
+
+let valueInNumber = Number (age)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+        
+o/p
+object
+object
+number
+0        */
+
+
+/*let point =undefined
+
+
+console.log(typeof point);
+console.log(typeof (point));
+
+
+let valueInNumber = Number (point)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+o/p 
+undefined
+undefined
+number
+NaN */
+
+
+//"33" =>33
+
+//"33abc" => NAN
+
+// true => 1; false => 0
+
+
+
+/*let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+o/p
+true    */
+
+
+/*let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+o/p
+false*/
+
+/*let isLoggedIn = "Hariom"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+o/p 
+true */
+
+// 1 => true ; o => false
+// "" => false
+// "Hariom" => true
+
+
+let someNumber = 33
+
+let stringNumber = String (someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+//o/p 
+//33
+//string
