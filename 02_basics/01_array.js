7e992dd029949bc767bc212e72c136249b072dd8
Hariom Kabra <143120733+Hariomkabra@users.noreply.github.com>
2024-10-18 14:16:16 +0000

    some change

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,39 +1,40 @@
 //array
 
-//const myArr1=[0,1,2,3,4,5] // Array object,as with array in others programing languages,enables storing a collection of multiple items under a single variable,name and has memeber for perform sommon aray operations.  Javascript array is resizable.
-//const myHeros=["Ram","Sham"]
+const myArr1=[0,1,2,3,4,5] // Array object,as with array in others programing languages,enables storing a collection of multiple items under a single variable,name and has memeber for perform sommon aray operations.  Javascript array is resizable.
+const myHeros=["Ram","Sham"]
 
-//const myArr2=new Array(1,2,3,4)
-//console.log(myArr2[0]); // o/p 0
+const myArr2=new Array(1,2,3,4)
+console.log(myArr2[0]); // o/p 0
 
-//Array method
+//array method
 const myArr=[0,1,2,3,4,5]
-//myArr.push(6);
-//console.log(myArr); // o/p [0,1,2,3,4,5,6]
+myArr.push(6);
+console.log(myArr); // o/p [0,1,2,3,4,5,6]
 
-//myArr.push(7);
-//console.log(myArr); // o/p [0,1,2,3,4,5,6,7]
+myArr.push(7);
 
-//myArr.pop();
-//console.log(myArr); // o/p [0,1,2,3,4,5,6]
+console.log(myArr); // o/p [0,1,2,3,4,5,6,7]
 
-//myArr.unshift(0)
-//console.log(myArr);// o/p [ 0, 0, 1, 2, 3, 4, 5]
+myArr.pop();
+console.log(myArr); // o/p [0,1,2,3,4,5,6]
 
-//myArr.unshift(9)
-//console.log(myArr);// o/p [ 9, 0, 1, 2, 3, 4, 5]
+myArr.unshift(0)
+console.log(myArr);// o/p [ 0, 0, 1, 2, 3, 4, 5]
 
-//myArr.shift()
-//console.log(myArr);// o/p [ 1, 2, 3, 4, 5]
+myArr.unshift(9)
+console.log(myArr);// o/p [ 9, 0, 1, 2, 3, 4, 5]
 
-//console.log(myArr.includes(9)); // o/p "false"
-//console.log(myArr.indexOf(2)); // o/p 2
+myArr.shift()
+console.log(myArr);// o/p [ 1, 2, 3, 4, 5]
 
-//const newArr = myArr.join()
+console.log(myArr.includes(9)); // o/p "false"
+console.log(myArr.indexOf(2)); // o/p 2
 
-//console.log(myArr);
+const newArr = myArr.join()
 
-//console.log(typeof newArr);  /// o/p "String"
+console.log(myArr);
+
+console.log(typeof newArr);  /// o/p "String"
 
 
 //Slice , Splice
