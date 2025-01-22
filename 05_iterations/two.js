e1f1d731310e2c4e3c19ded02086c9f09ce60378
Hariom Kabra <143120733+Hariomkabra@users.noreply.github.com>
2025-01-22 05:53:58 +0000

    while Do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+
+let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// } o/p 0 2 4 6 8 10
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    //console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}  // o/p flash batman superman
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);  // o/p Score is 11
