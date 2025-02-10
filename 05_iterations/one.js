a76214a2f13597da45befb707f7a7691736adf81
Hariom Kabra <143120733+Hariomkabra@users.noreply.github.com>
2025-02-10 22:52:52 +0530

    Update one.js

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -44,6 +44,7 @@ for (let i = 0; i < myArray.length; i++) {
 for (let index = 1; index <= 20; index++) {
     if (index == 5) {
         console.log(`Detected 5`);  // Output: Detected 5
+      
        continue
    }
   console.log(`Value of i is ${index}`);
